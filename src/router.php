54c562adc86bebdf454436aeb350a96d7872b100
Wojtazz <marcin.witas72@gmail.com>
2024-10-27 10:08:42 +0100

    feat: passing route params to controllers

--- src/router.php
+++ src/router.php
@@ -3,18 +3,16 @@
 declare(strict_types=1);
 
 use Mmconsulting\Modules\Invoices\Controllers\InvoiceController;
+use function FastRoute\simpleDispatcher;
+use FastRoute\RouteCollector;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $router) {
-	$router->addRoute('GET', '/overpayments', [InvoiceController::class, 'overpayments']);
+$dispatcher = simpleDispatcher(function (RouteCollector $router) {
+	$router->addRoute('GET', '/overpayments/{client_id:\d+}', [InvoiceController::class, 'overpayments']);
 });
 
-// Fetch method and URI from somewhere
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-// Strip query string (?foo=bar) and decode URI
 if (false !== $pos = strpos($uri, '?')) {
 	$uri = substr($uri, 0, $pos);
 }
@@ -27,10 +25,11 @@ switch ($routeInfo[0]) {
 	case FastRoute\Dispatcher::NOT_FOUND:
 		echo 'ERROR 404';
 		break;
+
 	case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-//		$allowedMethods = $routeInfo[1];
 		echo 'ERROR 405';
 		break;
+
 	case FastRoute\Dispatcher::FOUND:
 		[$status, $handler, $vars] = $routeInfo;
 
@@ -38,7 +37,17 @@ switch ($routeInfo[0]) {
 			if (method_exists($handler[0], $handler[1])) {
 				$controller = new $handler[0]();
 
-				$controller->{$handler[1]}();
+				$params = [];
+
+				foreach ($vars as $key => $value) {
+					if (is_numeric($value)) {
+						$params[$key] = (int) $value;
+					} else {
+						$params[$key] = $value;
+					}
+				}
+
+				$controller->{$handler[1]}(...$params);
 			}
 		}
 
