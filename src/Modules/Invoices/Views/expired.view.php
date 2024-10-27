a9a5587e2e6b4ae1ff3e4f80aa262cac75759bb3
Wojtazz <marcin.witas72@gmail.com>
2024-10-27 18:36:26 +0100

    feat: add button for going to previous page

--- src/Modules/Invoices/Views/expired.view.php
+++ src/Modules/Invoices/Views/expired.view.php
@@ -2,7 +2,10 @@
     <h1 class="text-3xl font-semibold text-white mb-4">Nierozliczone faktury po terminie płatności</h1>
 
     <?php if (empty($client)): ?>
-        <span>Brak danych.</span>
+        <div class="flex flex-col gap-y-4">
+            <span>Brak danych.</span>
+            <button type="button" class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800" onclick="history.back()">Powrót</button>
+        </div>
     <?php else: ?>
         <div class="flex flex-col gap-y-10">
 			<?php require_once('Elements/filters.view.php'); ?>
